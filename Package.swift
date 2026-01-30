68c900efa7a1dbfde0b83b042af6cb17b3a853fc
Warren <warren@modelhealth.io>
2026-01-30 18:50:30 +0700

    Moved source to internal

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "ModelHealth",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "ModelHealth",
+            targets: ["ModelHealth"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ModelHealthFFI",
+            path: "ModelHealthFFI.xcframework"
+        ),
+        .target(
+            name: "ModelHealth",
+            dependencies: ["ModelHealthFFI"],
+            path: "Sources/ModelHealth"
+        ),
+    ]
+)
